2efdca873b5e3aa00de2076257d123ad40131b4d
nadavh <nadahb@gmail,com>
2023-05-01 20:56:56 +0300

    almost everything in assignment 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,139 @@
-import React from "react";
+import React, { useState } from "react";
 import type { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
 import prisma from '../lib/prisma'
+import { type } from "os";
+import Link from "next/link";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
-    include: {
-      author: {
-        select: {
-          name: true,
+const Pagination = ({ items,        pageSize,         currentPage,         onPageChange }: 
+                    {items: number, pageSize: number, currentPage: number, onPageChange: (page: number) => void}) => {
+  const pagesCount = Math.ceil(items / pageSize); // 100/10
+ 
+  if (pagesCount === 1) return null;
+  const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
+ console.log(pages)
+ 
+  return (
+    <div>
+      <div>Pagination</div>
+    </div>
+  );
+ };
+
+
+type Nullable<T> = T | null;
+
+type Posts = {
+  id: number,
+  title: string,
+  published: boolean,
+  author: Nullable<{name: Nullable<string>}>,
+  authorId: Nullable<number>,
+  content: Nullable<string>
+}[]
+
+type Post = {
+  id: number,
+  title: string,
+  published: boolean,
+  author: Nullable<{name: Nullable<string>}>,
+  authorId: Nullable<number>,
+  content: Nullable<string>
+}
+
+
+const postsPerPage = 10;
+
+
+export const getServerSideProps: GetServerSideProps = async ({ query: { page = '1' } }) => {
+  if(Array.isArray(page))
+    return {props: []};
+
+  const pageIndex = parseInt(page);
+
+  const feed = await prisma.$queryRaw<{id: number}[]>`SELECT id FROM Post where id >
+  (
+    SELECT COALESCE(max(id), -1) from 
+    (
+      SELECT id from Post
+      WHERE published = 1
+      ORDER BY id
+      LIMIT ${postsPerPage * (pageIndex - 1)}
+    )	
+  )
+  AND published = 1
+  LIMIT ${postsPerPage}`
+  .then(ids => 
+    prisma.post.findMany({
+      where: {
+        id: {in: ids.map(id => id.id)}
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
+          },
         },
       },
-    },
-  });
+    })
+  );
+
+  var totalPages = await prisma.post.count({
+    where: {published: true}
+  }) / postsPerPage;
+
+
+  const flooredTotalPages = Math.floor(totalPages);
+
+  if(totalPages > flooredTotalPages) {
+    totalPages = flooredTotalPages + 1;
+  }
+
   return {
-    props: { feed },
-  };
-};
+    props: { feed, page: pageIndex, totalPages }
+  }
+
+}
+
 
 type Props = {
+  page: number;
   feed: PostProps[];
+  totalPages: number;
 };
 
+
+
 const Blog: React.FC<Props> = (props) => {
+  function nextPage() {
+    if(props.page === props.totalPages) {
+      return <span>Next</span>
+    }
+    return <Link href={`/?page=${props.page+1}`}>Next</Link>
+  } 
+  function previousPage() {
+    if(props.page === 1) {
+      return <span>Previous</span>
+    }
+    return <Link href={`/?page=${props.page-1}`}>Previous</Link>
+  }
+
+  const navigationButtons = <React.Fragment>
+  {previousPage()}
+  <span> </span>
+  <span> </span>
+  <span> </span>
+  {nextPage()}    
+  </React.Fragment>;
+
+
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
+        <h2>Page {props.page} / {props.totalPages}</h2>
+        {navigationButtons}
         <main>
           {props.feed.map((post) => (
             <div key={post.id} className="post">
@@ -38,6 +141,7 @@ const Blog: React.FC<Props> = (props) => {
             </div>
           ))}
         </main>
+        {navigationButtons}
       </div>
       <style jsx>{`
         .post {
@@ -52,6 +156,7 @@ const Blog: React.FC<Props> = (props) => {
         .post + .post {
           margin-top: 2rem;
         }
+        
       `}</style>
     </Layout>
   );
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,16 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma, Post } from '@prisma/client'
+import { SingleBar, Presets } from 'cli-progress'
+import tweets from '../tweets.json'
+
+const nyanProgress = require('nyan-progress')
+// import * as nyanProgress from 'nyan-progress'
+
+
 
 const prisma = new PrismaClient()
+
+const rangeFrom = (start: number, end: number) => Array.from({length: (end - start)}, (v, k) => k + start);
+const range = (end: number) => rangeFrom(0, end);
 
 const userData: Prisma.UserCreateInput[] = [
   {
@@ -48,14 +58,122 @@ const userData: Prisma.UserCreateInput[] = [
   },
 ]
 
+export type BasicTweet = {
+  text: string,
+  time: string
+};
+
+
+
+
+export type Tweet = {
+  title: string,
+  content: string,
+  published: boolean,
+  authorId: number
+};
+
+const btToTweet = (bt: BasicTweet, authorId: number): Tweet => ({
+  title: `Tweet by @nHaik at ${bt.time}`,
+  content: bt.text,
+  published: randomElement([false, true]),
+  authorId: authorId
+});
+
+
+const getAllTweets = (authorId: number): Tweet[] => tweets.map(bt => btToTweet(bt, authorId));
+
+const randomElement = <T> (array: T[]): T => 
+  array[Math.floor(Math.random() * array.length)];
+
+
+type LoadingBar = "NyanCat" | "SquareBar";
+
+const barType: LoadingBar = "NyanCat";
+
+const numberOfRandomPosts = 100000;
+const postsPerBulk = 100;
+var nyanBar: any;
+var sqruareBar: SingleBar;
+
+function startBar(numberOfRandomPosts: number) {
+  if(barType === "NyanCat") {
+    nyanBar = nyanProgress();
+    nyanBar.start({total: numberOfRandomPosts});
+  }
+  else {
+    sqruareBar = new SingleBar({}, Presets.shades_classic)
+    sqruareBar.start(numberOfRandomPosts, 0);
+  }
+}
+
+function tickBar() {
+  if(barType === "NyanCat")
+    nyanBar.tick();
+  else
+    sqruareBar.increment();
+}
+
+function stopBar() {
+  if(barType === "NyanCat")
+    nyanBar.terminate();
+  else  {
+    sqruareBar.stop();
+  }
+}
+
+
+const tweetRandomly = async (userId: number): Promise<void> => {
+  const allTweets =  getAllTweets(userId);
+  // const loadingBar = new SingleBar({}, Presets.shades_classic);
+  // loadingBar.start(numberOfRandomPosts, 0);
+  console.log('Tweeting randomly...');
+  startBar(numberOfRandomPosts);
+  let totalInserted = 0;
+
+  while(totalInserted < numberOfRandomPosts) {
+    const postPromises: Promise<void>[] = [];
+    const currentBulkSize = Math.min(postsPerBulk, numberOfRandomPosts - totalInserted);
+    await range(currentBulkSize).forEach(async () => {
+      try {
+        postPromises.push(prisma.post.create({data: randomElement(allTweets)})
+        .then(tickBar));
+      } catch(e) {
+        console.warn('Could not create post - retrying.');
+        postPromises.push(prisma.post.create({data: randomElement(allTweets)})
+          .then(tickBar));
+      }
+      totalInserted++;
+    })
+    await Promise.all(postPromises);
+  }
+  stopBar();
+  // loadingBar.stop();
+}
+
 async function main() {
   console.log(`Start seeding ...`)
+
   for (const u of userData) {
     const user = await prisma.user.create({
       data: u,
+      
     })
     console.log(`Created user with id: ${user.id}`)
   }
+
+
+    const nahab = await prisma.user.create({
+      data: {
+        name: 'נחב בטוויטר',
+        email: 'nadavhb@gmail.com',
+        posts: {create: []}
+      }
+    });
+    console.log(`Created nahab with id: ${nahab.id}`)
+    await tweetRandomly(nahab.id);
+    // console.log(await prisma.post.findMany());
+  
   console.log(`Seeding finished.`)
 }
 
